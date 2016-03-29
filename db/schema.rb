4bc542f0f081495138bf9a62b911e2a0a6d8355f
snopcoff <snoopy.8994@gmail.com>
2016-03-29 20:00:26 +0000

    Add Upload Image

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160327114825) do
+ActiveRecord::Schema.define(version: 20160329192601) do
+
+  create_table "images", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.string   "title"
+    t.string   "detail"
+    t.string   "file"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
