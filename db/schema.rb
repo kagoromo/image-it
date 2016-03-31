67428bef7b504387ff6a61816c46b63746c22013
snopcoff <snoopy.8994@gmail.com>
2016-03-31 06:07:45 +0000

    Avatar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160330081348) do
+ActiveRecord::Schema.define(version: 20160331054519) do
 
   create_table "images", force: :cascade do |t|
     t.integer  "user_id"
